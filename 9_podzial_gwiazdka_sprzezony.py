a950424c07fd0729541239015d3955ba3c8f6331
Mateusz Gałganek <mateusz.maciej.galganek@gmail.com>
2014-10-20 19:02:19 +0200

    ...

--- 9_podzial_gwiazdka_sprzezony.py
+++ 9_podzial_gwiazdka_sprzezony.py
@@ -1,12 +1,23 @@
+def podzial_sprzezony(a):
+    b = [1 for _ in range(1, a[1])]
+    m = a[1]
+    for j in range(2, m):
+        for i in range(1, a[j]):
+            b[i] = b[i] + 1
+    return b
+
+
 a = []
 
 
 def podzial_gwiazdka(n, b, m):
     global a
+
     if a == []:
         a = [0 for _ in range(1, min(b, n))]
+        a[1] = b
     if n == 0:
-        print a[1:-1]
+        print podzial_sprzezony(a[1:-1])
     else:
         for i in range(1, min(b, n + 1)):
             a[m + 1] = i
