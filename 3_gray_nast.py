47ac798d63fcd4e8a7c3487c1c0d4709e0e80195
Mateusz Gałganek <mateusz.maciej.galganek@gmail.com>
2014-10-24 19:15:42 +0200

    Fixed gray

--- 3_gray_nast.py
+++ 3_gray_nast.py
@@ -7,11 +7,12 @@ def gray_nast(n, T):
         U = T.symmetric_difference([n])
     else:
         j = n
+
         while j not in T:
             j = j - 1
             if j == 1:
                 return None
-            U = T.symmetric_difference([j - 1])
+        U = T.symmetric_difference([j - 1])
     return list(U)
 
 lists = [
@@ -27,10 +28,10 @@ lists = [
 for list_unrank in lists:
     print gray_nast(3, list_unrank)
 # [3]
-# []
+# [2, 3]
 # [2]
 # [1, 2]
 # [1, 2, 3]
-# []
+# [1, 3]
 # [1]
 # None
