01a811427cfc476845764b975aabd57af642d8de
Mateusz Gałganek <mateusz.maciej.galganek@gmail.com>
2014-10-30 20:30:44 +0100

    Add desc

--- 9_podzial_gwiazdka_sprzezony.py
+++ 9_podzial_gwiazdka_sprzezony.py
@@ -8,6 +8,21 @@ b = k
 """
 
 
+def remove():
+    """  """
+    koniec = True
+    while koniec:
+        while 1 in a:
+            a.remove(1)
+        koniec = False
+
+
+def add(m):
+    """Dodajemy do listy kolejny element"""
+    while m + 1 >= len(a):
+        a.append(0)
+
+
 def podzial_sprzezony(c):
     b = [1 for _ in range(1, c[1])]
     m = c[1]
@@ -21,19 +36,16 @@ a = []
 
 
 def podzial_gwiazdka(n, b, m):
-    global a
-
-    if a == []:
-        a = [0 for _ in range(1, min(b, n))]
-        a[1] = b
     if n == 0:
-        print podzial_sprzezony(a[1:-1])
+        print (a[1:])
+        remove()
     else:
         for i in range(1, min(b, n + 1)):
+            add(m)
             a[m + 1] = i
             podzial_gwiazdka(n - i, i, m + 1)
 
 podzial_gwiazdka(6, 6, 0)
-# [1]
-# [1]
-# []
+# [3, 2, 1]
+# [4, 2]
+# [5, 1]
