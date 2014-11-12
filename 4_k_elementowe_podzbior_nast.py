49f14b404c86c5add7805dccea2cbf0cf3a675a9
Mateusz Gałganek <mateusz.maciej.galganek@gmail.com>
2014-11-12 16:46:15 +0100

    Fix algoritm

--- 4_k_elementowe_podzbior_nast.py
+++ 4_k_elementowe_podzbior_nast.py
@@ -9,7 +9,7 @@ możliwego elementu.
 
 
 def k_elementowe_podzbiory_nast(T, k, n):
-    U = T
+    U = T[:]
     i = k
     while i >= 1 and T[i] == n - k + i:
         i = i - 1
@@ -36,13 +36,13 @@ unranks = [
 for unrank in unranks:
     # empty list for zero index
     print (k_elementowe_podzbiory_nast([[]] + unrank, 3, 5))
-# [1, 2, 4]
-# [1, 2, 5]
-# [1, 3, 5]
-# [1, 3, 5]
-# [1, 4, 6]
-# [2, 4, 5]
-# [2, 3, 5]
-# [2, 4, 6]
-# [3, 5, 6]
+# [[], 1, 2, 4]
+# [[], 1, 2, 5]
+# [[], 1, 3, 4]
+# [[], 1, 3, 5]
+# [[], 1, 4, 5]
+# [[], 2, 3, 4]
+# [[], 2, 3, 5]
+# [[], 2, 4, 5]
+# [[], 3, 4, 5]
 # None
