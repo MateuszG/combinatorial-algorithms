5c9ba7d128ca88893cf58c2f34e0b31b004bc7d8
Mateusz Gałganek <mateusz.maciej.galganek@gmail.com>
2014-11-17 19:06:14 +0100

    Improve desc

--- 8_generuj_rgf.py
+++ 8_generuj_rgf.py
@@ -2,17 +2,17 @@
 Algorytm 18
 1) Stwórz listę 'f' wypełnioną 1 i listę 'F' wypełnioną 2 (1...n).
 2) Zdefiniuj zmienną boolowską 'koniec' i przypisz jej False.
-3) Dopóki 'koniec' nie jest True to wypisuj tablicę 'f'.
-4) Przypisz 'j' wartośc 'n', jeśli f[j] nie jest równe F[j] to zmniejszaj 'j'
-o 1.
+3) Dopóki 'koniec' nie jest True, to wypisuj tablicę 'f'.
+4) Przypisz 'j' wartośc 'n', jeśli f[j] jest równe F[j] to zmniejszaj 'j' o 1.
 5) Gdy wartość 'j' jest większa od 1 to wartość elementu f[j] zostaje
-zwiększona o 1 a wszystkie elementy leżące na prawo od przypisujemy 1,
+zwiększona o 1, a wszystkie elementy leżące na prawo od przypisujemy 1,
 uaktualniając jednocześnie tablice F.
 6) Uaktualnienie to polega na tym, że jeżeli nowa wartość f[j] daje
 f[j] == F[j], to wszystkie elementy tablicy F leżące na prawo przyjmują
 wartość F[j] + 1, a w przeciwnym razie przyjmują one wartość F[j].
-7) Proces ten kontynujemy dopóki nie będzie spełniony warunek f[j] == F[j] dla
-wszystkich j.
+7) Proces (3-6) ten kontynujemy, dopóki nie będzie spełniony warunek
+f[j] == F[j] dla wszystkich 'j', czyli 'j' będzie mniejsze lub równe 1, gdy to
+nastąpi to koniec = True
 """
 
 
@@ -21,7 +21,7 @@ def generuj_rgf(n):
     F = [2 for _ in range(n)]
     koniec = False
     while not koniec:
-        print f
+        print (f)
         j = n
         while True:
             j = j - 1
