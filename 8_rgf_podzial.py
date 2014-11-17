3e755b499ed4fec817f7ba9331ecfc69de0b1124
Mateusz Gałganek <mateusz.maciej.galganek@gmail.com>
2014-11-17 14:00:40 +0100

    Improve desc

--- 8_rgf_podzial.py
+++ 8_rgf_podzial.py
@@ -1,10 +1,11 @@
 """
 Algorytm 17
-1) Pierwszy 'for' wyznacza, na podstawie zadanej funkcji RGF, liczbe bloków w
-podziale odpowiadającym tej funkcji. Sprowadza się to do wyznaczenia
-największej składowej.
-2) W ostatniej pętli 'for' wstawiane są do kolejnych bloków podziału stosowne
-elementy.
+1) k = 1
+2) Szukamy największego F[j] (1...n) porównując go z wartością 'k', jeśli
+znajdziemy większą wartość to przypisujemy 'k' tą wartość F[j].
+3) Tworzymy pustą listę 'B' z podlistami (1...k).
+4) Iterując 'j' (1...n) wartości F[j] potraktuj jako indeksy listy B,
+dodając do podzbioru B[F[j]] wartość [j].
 """
 
 
@@ -19,7 +20,7 @@ def rgf_podzial(n, F):
     for j in range(1, n + 1):
         B[F[j]] = B[F[j]] + [j]
 
-    B = [x for x in B if x != []]  # Remove empty lists in list
+    B = [x for x in B if x != []]  # Remove empty lists in list B
     return B
 
 
@@ -66,4 +67,9 @@ for val in values:
 """
 Procedura wyznaczająca z zadanej funkcji RGF(f[1] , . . . , f[n])
 odpowiadający jej podział zbioru {1, 2, . . . , n} na stosowna liczbe bloków.
+1) Pierwszy 'for' wyznacza, na podstawie zadanej funkcji RGF, liczbe bloków w
+podziale odpowiadającym tej funkcji. Sprowadza się to do wyznaczenia
+największej składowej.
+2) W ostatniej pętli 'for' wstawiane są do kolejnych bloków podziału stosowne
+elementy.
 """
