7bb5f8472bb93ec7379dff9461b6cd76f1cd6abd
Mateusz Gałganek <mateusz.maciej.galganek@gmail.com>
2014-11-17 16:51:14 +0100

    Improve desc

--- 8_generuj_rgf.py
+++ 8_generuj_rgf.py
@@ -1,13 +1,17 @@
 """
 Algorytm 18
-1) Idea algorytmu polega na znalezieniu pierwszej pozycji z prawej
-strony tablicy f, dla której f[j] != F[j].
-2) Wówczas wartość elementu f[j] zostaje zwiększona o 1 a wszystkie elementy
-leżące na prawo od niego czynimy równe 1, uaktualniając jednocześnie tablice F.
-3) Uaktualnienie to polega na tym, że jeżeli nowa wartość f[j] daje
+1) Stwórz listę 'f' wypełnioną 1 i listę 'F' wypełnioną 2 (1...n).
+2) Zdefiniuj zmienną boolowską 'koniec' i przypisz jej False.
+3) Dopóki 'koniec' nie jest True to wypisuj tablicę 'f'.
+4) Przypisz 'j' wartośc 'n', jeśli f[j] nie jest równe F[j] to zmniejszaj 'j'
+o 1.
+5) Gdy wartość 'j' jest większa od 1 to wartość elementu f[j] zostaje
+zwiększona o 1 a wszystkie elementy leżące na prawo od przypisujemy 1,
+uaktualniając jednocześnie tablice F.
+6) Uaktualnienie to polega na tym, że jeżeli nowa wartość f[j] daje
 f[j] == F[j], to wszystkie elementy tablicy F leżące na prawo przyjmują
 wartość F[j] + 1, a w przeciwnym razie przyjmują one wartość F[j].
-4) Proces ten kontynujemy dopóki nie będzie spełniony warunek f[j] == F[j] dla
+7) Proces ten kontynujemy dopóki nie będzie spełniony warunek f[j] == F[j] dla
 wszystkich j.
 """
 
@@ -16,28 +20,21 @@ def generuj_rgf(n):
     f = [1 for _ in range(n)]
     F = [2 for _ in range(n)]
     koniec = False
-
     while not koniec:
         print f
         j = n
-
         while True:
             j = j - 1
-
             if f[j] != F[j]:
                 break
-
         if j > 0:
             f[j] = f[j] + 1
-
             for i in range(j + 1, n):
                 f[i] = 1
-
                 if f[j] == F[j]:
                     F[i] = F[j] + 1
                 else:
                     F[i] = F[j]
-
         else:
             koniec = True
 
@@ -57,3 +54,7 @@ generuj_rgf(4)
 # [1, 2, 3, 2]
 # [1, 2, 3, 3]
 # [1, 2, 3, 4]
+"""
+1) Idea algorytmu polega na znalezieniu pierwszej pozycji z prawej
+strony tablicy f, dla której f[j] != F[j].
+"""
